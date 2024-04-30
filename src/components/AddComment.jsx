5cf8c040aa153a28d7ebd3825662a6ce8f7b1873
Matteo Moscardini <160152042+matte-mosca@users.noreply.github.com>
2024-04-30 16:43:58 +0200

    epibooks

---
+++ src/components/AddComment.jsx
@@ -0,0 +1,92 @@
+import { useEffect, useState } from "react";
+import { Button, Form } from "react-bootstrap";
+
+const AddComment = ({ asin }) => {
+  const [comment, setComment] = useState({
+    comment: "",
+    rate: 1,
+    elementId: null,
+  });
+
+  useEffect(() => {
+    setComment((c) => ({
+      ...c,
+      elementId: asin,
+    }));
+  }, [asin]);
+
+  const sendComment = async (e) => {
+    e.preventDefault();
+    try {
+      let response = await fetch(
+        "https://striveschool-api.herokuapp.com/api/comments",
+        {
+          method: "POST",
+          body: JSON.stringify(comment),
+          headers: {
+            "Content-type": "application/json",
+            Authorization:
+              "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2NjE4ZjA4MDdmMzA0NjAwMWFlNTlmODgiLCJpYXQiOjE3MTQ0ODY0OTQsImV4cCI6MTcxNTY5NjA5NH0.YaE3Kf807PJFd_sSzQSnhA9CNdm6EDuKnJlMTKh4lHA",
+          },
+        }
+      );
+      if (response.ok) {
+        alert("Recensione inviata!");
+        setComment({
+          comment: "",
+          rate: 1,
+          elementId: null,
+        });
+      } else {
+        throw new Error("Qualcosa è andato storto");
+      }
+    } catch (error) {
+      alert(error);
+    }
+  };
+
+  return (
+    <div className="my-3">
+      <Form onSubmit={sendComment}>
+        <Form.Group className="mb-2">
+          <Form.Label>Recensione</Form.Label>
+          <Form.Control
+            type="text"
+            placeholder="Inserisci qui il testo"
+            value={comment.comment}
+            onChange={(e) =>
+              setComment({
+                ...comment,
+                comment: e.target.value,
+              })
+            }
+          />
+        </Form.Group>
+        <Form.Group className="mb-2">
+          <Form.Label>Valutazione</Form.Label>
+          <Form.Control
+            as="select"
+            value={comment.rate}
+            onChange={(e) =>
+              setComment({
+                ...comment,
+                rate: e.target.value,
+              })
+            }
+          >
+            <option>1</option>
+            <option>2</option>
+            <option>3</option>
+            <option>4</option>
+            <option>5</option>
+          </Form.Control>
+        </Form.Group>
+        <Button variant="primary" type="submit">
+          Invia
+        </Button>
+      </Form>
+    </div>
+  );
+};
+
+export default AddComment;
---
+++ src/components/Error.jsx
@@ -0,0 +1,8 @@
+import { Alert } from 'react-bootstrap'
+
+const Error = () => (
+  <Alert variant="danger">Errore - Bearer token non inserito</Alert>
+)
+
+export default Error
+
